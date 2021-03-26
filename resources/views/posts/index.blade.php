ef676fa8913ada0644288e4fd0eaf7d406a37931
Muhammed Abdurahman <muhammed.abdurahman93@gmail.com>
2021-03-26 23:44:59 +0200

    I am done

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="d-flex">
+    <a href="{{route('posts.create')}}" class="mx-auto btn btn-success">Create Post</a>
+  </div>
+
+  <table class="table mt-5">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">title</th>
+          <th scope="col">slug</th>
+        <th scope="col">posted by</th>
+        <th scope="col">created at</th>
+        <th scope="col">actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($posts as $post)
+      <tr>
+        <th scope="row">{{$post->id}}</th>
+        <td>{{$post->title}}</td>
+          <td>{{$post->slug}}</td>
+        <td>{{$post->user ? $post->user->name : 'user not found'}}</td>
+        <td>{{$post->created_at->format('Y-m-d')}}</td>
+        <td class="col" style="display: flex;">
+          <a href="{{ route('posts.show', [ 'post' => $post['id'] ]) }}" class="btn btn-info" style="margin-right: 5px;">View</a>
+          <a href="{{route('posts.edit', ['post' => $post['id'] ])}}" class="btn btn-primary mr-1">Edit</a>
+          <form method="post" action="{{route('posts.destroy' , ['post'=>$post['id']])}}">
+            @csrf
+            {{ method_field('DELETE') }}
+            <input type="submit" value="Delete" class="btn btn-danger" onclick="return confirm('Are you sure ?')">
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  {{$posts->links()}}
+</div>
+@endsection
